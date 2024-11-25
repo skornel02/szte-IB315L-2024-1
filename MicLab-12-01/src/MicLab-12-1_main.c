ef66ef62cdcc11d09ad639d4867ad0b169913085
Stefán Kornél <stefankornel02@gmail.com>
2024-11-25 22:34:13 +0100

    12. labor

--- MicLab-12-01/src/MicLab-12-1_main.c
+++ MicLab-12-01/src/MicLab-12-1_main.c
@@ -1,21 +1,10 @@
-//-----------------------------------------------------------------------------
-// Includes
-//-----------------------------------------------------------------------------
-#include <SI_EFM8BB1_Register_Enums.h>                // SFR declarations
+#include <SI_EFM8BB1_Register_Enums.h>
 
 #define WD_DISABLE_A 0xDE
 #define WD_DISABLE_B 0xAD
 
 #define LED0 P1_B4
 
-//-----------------------------------------------------------------------------
-// SiLabs_Startup() Routine
-// ----------------------------------------------------------------------------
-// This function is called immediately after reset, before the initialization
-// code is run in SILABS_STARTUP.A51 (which runs before main() ). This is a
-// useful place to disable the watchdog timer, which is enable by default
-// and may trigger before main() in some instances.
-//-----------------------------------------------------------------------------
 void SiLabs_Startup (void)
 {
   // Disable the watchdog here.
@@ -29,20 +18,9 @@ void SiLabs_Startup (void)
   P1MDOUT |= P1MASK_B4__COMPARED;
 }
 
-//-----------------------------------------------------------------------------
-// main() Routine
-// ----------------------------------------------------------------------------
-// Note: the software watchdog timer is not disabled by default in this
-// example, so a long-running program will reset periodically unless
-// the timer is disabled or your program periodically writes to it.
-//
-// Review the "Watchdog Timer" section under the part family's datasheet
-// for details. To find the datasheet, select your part in the
-// Simplicity Launcher and click on "Data Sheet".
-//-----------------------------------------------------------------------------
 int main (void)
 {
   LED0 = 0;
 
-  while (1) {}                             // Spin forever
+  while (1) {}
 }
--- MicLab-12-02/src/MicLab-12-02_main.c
+++ MicLab-12-02/src/MicLab-12-02_main.c
@@ -1,7 +1,4 @@
-//-----------------------------------------------------------------------------
-// Includes
-//-----------------------------------------------------------------------------
-#include <SI_EFM8BB1_Register_Enums.h>                // SFR declarations
+#include <SI_EFM8BB1_Register_Enums.h>
 #include <SI_EFM8BB1_Defs.h>
 
 #define WD_DISABLE_A 0xDE
@@ -15,22 +12,13 @@
 #define TIMER3_TARGET_HZ 10u
 #define TIMER3_SIZE 65536u
 #define TIMER3_RELOAD (TIMER3_SIZE - (TIMER3_BASE_CLOCK / TIMER3_TARGET_HZ))
-// Ez csak 8.17 Hz képlet szerint
+
+#define BYTE_SIZE 8u
 
 #define LED0 P1_B4
 
-//-----------------------------------------------------------------------------
-// SiLabs_Startup() Routine
-// ----------------------------------------------------------------------------
-// This function is called immediately after reset, before the initialization
-// code is run in SILABS_STARTUP.A51 (which runs before main() ). This is a
-// useful place to disable the watchdog timer, which is enable by default
-// and may trigger before main() in some instances.
-//-----------------------------------------------------------------------------
 void SiLabs_Startup (void)
 {
-  // Disable the watchdog here
-
   // Erre nincs konstans az Enumsban.
   WDTCN = WD_DISABLE_A;
   WDTCN = WD_DISABLE_B;
@@ -46,24 +34,13 @@ void SiLabs_Startup (void)
   TMR3CN0 &= ~TMR3CN0_T3SPLIT__BMASK;
   TMR3CN0 &= ~TMR3CN0_T3XCLK__BMASK;
 
-  TMR3RLH = TIMER3_RELOAD >> 2;
+  TMR3RLH = TIMER3_RELOAD >> BYTE_SIZE;
   TMR3RLL = TIMER3_RELOAD;
 }
 
-//-----------------------------------------------------------------------------
-// main() Routine
-// ----------------------------------------------------------------------------
-// Note: the software watchdog timer is not disabled by default in this
-// example, so a long-running program will reset periodically unless
-// the timer is disabled or your program periodically writes to it.
-//
-// Review the "Watchdog Timer" section under the part family's datasheet
-// for details. To find the datasheet, select your part in the
-// Simplicity Launcher and click on "Data Sheet".
-//-----------------------------------------------------------------------------
 int main (void)
 {
-  while (1) {}                             // Spin forever
+  while (1) {}
 }
 
 void timer3_interrupt (void) interrupt TIMER3_IRQn
